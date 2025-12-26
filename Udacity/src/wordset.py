dc92ddca16b322af96113e8e99feb520a052d387
DIR\f.gorgojo.marugan <f.gorgojo.marugan@accenture.com>
2025-12-26 21:06:49 +0100

    Inicio del ejercicio Final

---
+++ Udacity/src/wordset.py
@@ -0,0 +1,16 @@
+
+english_word_small = {
+    "open",
+    "peon",
+    "nope",
+    "stone",
+    "notes",
+    "onset",
+    "tones",
+    "cone",
+    "pots",
+    "post",
+    "stop",
+    "opts",
+    "tops",
+}
